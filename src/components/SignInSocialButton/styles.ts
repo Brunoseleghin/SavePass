52d7308730fa1592e719947e93d7bf6449f7bea1
Bruno Seleghin <brunoseleghin@gmail.com>
2022-08-07 23:36:43 -0300

    Create component SignInSocialButton

---
+++ src/components/SignInSocialButton/styles.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components/native';
+
+import { RectButton } from 'react-native-gesture-handler';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+export const Button = styled(RectButton)`
+  height: ${RFValue(56)}px;
+
+  background-color: #FFCC00;
+  border-radius: 5px;
+
+  align-items: center;
+  flex-direction: row;
+
+  margin-bottom: 16px;
+`;
+
+export const ImageContainer = styled.View`
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+
+  padding: ${RFValue(16)}px;
+  border-color: #F0F2F5;
+  border-right-width: 1px;
+`;
+
+export const Text = styled.Text`
+  flex: 1;
+  text-align: center;
+
+  font-family: 'Rubik_500Medium';
+  color: #3D434D;
+  font-size: ${RFValue(14)}px;
+`;
