5931cd8506768bfec1865456985a0b6920ad7ca7
Bruno Seleghin <brunoseleghin@gmail.com>
2022-08-10 22:44:12 -0300

    Create global style

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,20 @@
+export default {
+  colors: {
+    primary: '#1967FB',
+    secondary: '#FFCC00',
+    white: '#FFFFFF',
+    white_ice: '#EBF2FF',
+    shape: '#F2F3F5',
+    gray: '#888D97',
+    blue_gray: '#3D434D',
+    error: '#E83F5B',
+    border_white: '#E3E4E5',
+    border_primary: '#508BFC',
+  },
+
+  fonts: {
+    light: 'Rubik_300Light',
+    regular: 'Rubik_400Regular',
+    medium: 'Rubik_500Medium'
+  }
+}
